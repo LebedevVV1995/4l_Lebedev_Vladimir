9a4a4830dbbb721860673d360ffa58745c872943
sgtKowalski <vovan-lvv2011@mail.ru>
2022-08-02 22:05:35 +0300

    Initial Commit

---
+++ 4l_Lebedev_Vladimir/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  4l_Lebedev_Vladimir
+//
+//  Created by Владимир on 02.08.2022.
+//  Copyright © 2022 Владимир. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
